b9301cf2f8b0901ddb035beb47eeaa05f3999314
Leeza2055 <leezashrestha999@gmail.com>
2022-01-07 14:01:03 +0545

    update, delete

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,9 +5,28 @@ class IdeasController < ApplicationController
 
     def create
         @idea = Idea.create(idea_params)
+        if @idea.valid?
+            #Implement later
+        else
+            #Implement later
         redirect_to root_path
+        end
     end
 
+    def update
+        @idea = Idea.find(params[:id])
+        if @idea.update(idea_params)
+            redirect_to root_path
+        else 
+            redirect_to edit_idea_path(params[:id])
+        end
+    end
+
+    def destroy
+        @idea = Idea.find(params[:id])
+        @idea.destroy
+        redirect_to root_path
+    end
     private
     def idea_params
         params.require(:idea).permit(:description, :author)
